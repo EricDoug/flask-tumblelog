46449afb4009874a5260746e0dfb41a9c2b50b8e
Zhiwei <code@zhiwei.li>
2013-12-20 01:19:23 +0800

    fix js/css and adapt to AppFog/MongoLab

--- tumblelog/__init__.py
+++ tumblelog/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+import os,urlparse
+
+
+MONGOLAB_URI = os.environ.get('MONGOLAB_URI')
+url = urlparse.urlparse(MONGOLAB_URI)
+MONGODB_USER = url.username
+MONGODB_PASSWORD = url.password
+MONGODB_HOST = url.hostname
+MONGODB_PORT = url.port
+MONGODB_DB = url.path[1:]
+
+
+
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {"DB": "my_tumble_log"}
+app.config["MONGODB_SETTINGS"] = {"DB": MONGODB_DB,
+    "USERNAME": MONGODB_USER,
+    "PASSWORD": MONGODB_PASSWORD,
+    "HOST"    : MONGODB_HOST,
+    "PORT"    : MONGODB_PORT
+    }
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 db = MongoEngine(app)
